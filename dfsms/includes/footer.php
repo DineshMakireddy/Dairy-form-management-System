feede9e3f7e79368f710447084076ccb16bf1411
dinesh <dineshmakireddy@gmail.com>
2023-08-19 09:36:12 +0530

    first commit

---
+++ dfsms/includes/footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+<p class='text-center'></p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
